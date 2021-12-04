6485e79d6b205278c583da21a8c8884f281d42c5
Carlos Aguirre Z <caaz160782@gmail.com>
2021-12-04 01:19:16 -0600

    add route for login

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const rolRouter = require("./rolRouter");
 const sizeRouter = require("./sizeRouter");
 const adminRouter= require("./adminRouter");
+const loginRouter= require("./loginRouter");
 
 const apiRouter = (app) => {
   app.use("/rols", rolRouter);
   app.use("/sizes", sizeRouter);
   app.use("/admin", adminRouter);
+  app.use("/login", loginRouter);
 
 };
 module.exports = apiRouter;

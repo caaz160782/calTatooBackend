b6b4591ca4559dad4781fc71ad2465d0a25672b5
Carlos Aguirre Z <caaz160782@gmail.com>
2021-12-03 19:01:41 -0600

    add route for admin

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const rolRouter = require("./rolRouter");
 const sizeRouter = require("./sizeRouter");
+const adminRouter= require("./adminRouter");
 
 const apiRouter = (app) => {
   app.use("/rols", rolRouter);
   app.use("/sizes", sizeRouter);
+  app.use("/admin", adminRouter);
+
 };
 module.exports = apiRouter;

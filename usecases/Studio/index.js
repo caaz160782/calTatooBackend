aee9be57f20ec5b5329f5983cdb66958ae4f9016
OmarTorres0607 <omartorres0607@gmail.com>
2021-12-05 14:03:42 -0600

    Added usecases
    Added display studio and modify studio

--- usecases/Studio/index.js
+++ usecases/Studio/index.js
@@ -1,8 +1,24 @@
-const tatooStudio = require("../../models/tatooStudio").model;
+const tatooStudio =require("../../models/tatooStudio").model;
 
+// Crear estudio
 const create = async (studioData) => {
     const {name,description,licenseImage,phone,social} = studioData;
     const studio = new Studio({id_user,name,description,licenseImage,phone,social});
     const savedStudio = await studio.save();
-    return  savedStudio
+    return  savedStudio;
 }
+
+// Ver estudios
+const get =async() =>{
+    const allStudio= await User.find({}).exec();
+    return allStudio;
+}
+
+const update =async (studioId, studioData) =>{
+    const{name,description,licenseImage,phone,social} =studioData;
+    return Post.findbyIdandUpdate(studioId,{name,description,licenseImage,phone,social}).exec();
+}
+
+
+
+module.exports = {create,get,update}

374ee1538573f339caab12dc7050e9430fd85d25
Carlos Aguirre Z <caaz160782@gmail.com>
2021-12-06 22:56:03 -0600

    se creo la ruta de infotatoo

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const rolRouter = require("./rolRouter");
 const sizeRouter = require("./sizeRouter");
 const adminRouter= require("./adminRouter");
 const loginRouter= require("./loginRouter");
+const tattoInfoRouter= require("./tattooInfoRouter");
 
 const apiRouter = (app) => {
   app.use("/rols", rolRouter);
   app.use("/sizes", sizeRouter);
   app.use("/admin", adminRouter);
   app.use("/login", loginRouter);
+  app.use("/tatooInfo", tattoInfoRouter);
 
 };
 module.exports = apiRouter;

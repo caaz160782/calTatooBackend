c3cc8d01eafe1ad52ad303eadff2e78d016e9bb0
OmarTorres0607 <omartorres0607@gmail.com>
2021-12-06 22:21:29 -0600

    Routers added
    Studio routes added, DB connected

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const rolRouter = require("./rolRouter");
 const sizeRouter = require("./sizeRouter");
 const adminRouter= require("./adminRouter");
+const studioRouter= require("./studioRouter")
 
 const apiRouter = (app) => {
   app.use("/rols", rolRouter);
   app.use("/sizes", sizeRouter);
   app.use("/admin", adminRouter);
-
+  app.use("/studio", studioRouter);
 };
 module.exports = apiRouter;

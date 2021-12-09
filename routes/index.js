eeec5243950f9aa0a5a5015ea3857d7e0d6d03d2
PaoZarateH <paolazarate2309@gmail.com>
2021-12-09 15:53:03 -0600

    se agrego ruta Clientes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ const rolRouter = require("./rolRouter");
 const sizeRouter = require("./sizeRouter");
 const adminRouter= require("./adminRouter");
 const loginRouter= require("./loginRouter");
+const clientRouter= require("./clientRouter")
 
 const apiRouter = (app) => {
   app.use("/rols", rolRouter);
   app.use("/sizes", sizeRouter);
   app.use("/admin", adminRouter);
   app.use("/login", loginRouter);
+  app.use("/client", clientRouter);
+  
 
 };
 module.exports = apiRouter;

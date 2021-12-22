eaaf94f187e98d613875a258a118e453dcc11da0
PaoZarateH <paolazarate2309@gmail.com>
2021-12-21 19:54:35 -0600

    Se modificaron rutas Clientes

---
+++ routes/clientModifiedRouter.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const client = require("../usecases/client");
+const { isClient } = require("../middlewares/authHandlers");
+
+//modificacion por el cliente
+router.patch('/:idClient', isClient, async (request,response,next) =>{
+    console.log(`request`,request)
+    const {idClient} = request.params;
+    const clientData = request.body
+    const  clientId = clientData._id
+
+    if(idClient === clientId)
+    {
+        try{
+           const clientUpdate=  await client.update(idClient,clientData);
+            response.status(201).json({
+                 ok: true,
+                 message: `Updated`,
+                 clientUpdate
+               })
+        }
+        catch (error){
+        next(error)
+        response.status(404).json({
+                status:false,
+                message:"Client not found"
+            })
+        }
+    }
+  else{
+    response.status(404).json({
+        ok:false,
+        message: "Can´t updated",
+
+    })
+  }
+});
+
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,21 @@ const rolRouter = require("./rolRouter");
 const sizeRouter = require("./sizeRouter");
 const adminRouter= require("./adminRouter");
 const loginRouter= require("./loginRouter");
-const clientRouter= require("./clientRouter")
+const clientAdminRouter= require("./clientAdminRouter");
 const staffRouter = require("./staffRouter");
 const tattoInfoRouter= require("./tattooInfoRouter");
 const studioRouter= require("./studioRouter")
+const clientModifiedRouter = require("./clientModifiedRouter");
 
 const apiRouter = (app) => {
   app.use("/rols", rolRouter);
   app.use("/sizes", sizeRouter);
   app.use("/admin", adminRouter);
   app.use("/login", loginRouter);
-  app.use("/client", clientRouter);
+  app.use("/clientAdmin", clientAdminRouter);
   app.use("/staff",staffRouter);
   app.use("/tatooInfo", tattoInfoRouter);
   app.use("/studio", studioRouter);
+  app.use("/clientModified", clientModifiedRouter);
 };
 module.exports = apiRouter;
